8dc3539488857b30dd6b20d5d407a762b0788cbc
xavierrmondragon <xavierrmondragon@gmail.com>
2023-02-02 11:17:35 +0100

    Exercises for section 2.3.

--- Work/report.py
+++ Work/report.py
@@ -35,20 +35,40 @@ def read_prices(filename):
     return prices
 
 
+def make_report(portfolio, prices):
+    report = []
+    for stock in portfolio:
+        name = stock['name']
+        shares = stock['shares']
+        price = prices[name]
+        change = price - stock['price']
+        report.append((name, shares, price, change))
+
+    return report
+
+
 portfolio = read_portfolio('Data/portfolio.csv')
 prices = read_prices('Data/prices.csv')
+report = make_report(portfolio, prices)
+
+# Print report
+headers = ('Name', 'Shares', 'Price', 'Change')
+print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
+print(('-' * 10 + ' ') * len(headers))
+for name, shares, price, change in report:
+    print(f'{name:>10s} {shares:>10d} {price:>10.2f} {change:>10.2f}')
 
 # Calculate the total cost of the portfolio
-total_cost = 0.0
-for s in portfolio:
-    total_cost += s['shares']*s['price']
+#total_cost = 0.0
+#for s in portfolio:
+#    total_cost += s['shares']*s['price']
 
-print('Total cost', total_cost)
+#print('Total cost', total_cost)
 
 # Compute the current value of the portfolio
-total_value = 0.0
-for s in portfolio:
-    total_value += s['shares']*prices[s['name']]
+#total_value = 0.0
+#for s in portfolio:
+#    total_value += s['shares']*prices[s['name']]
 
-print('Current value', total_value)
-print('Gain', total_value - total_cost)
+#print('Current value', total_value)
+#print('Gain', total_value - total_cost)
