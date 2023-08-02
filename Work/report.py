347e89c3f8351491c0324660ecb8856ab9acdd25
xavierrmondragon <xavierrmondragon@gmail.com>
2023-08-02 14:01:38 +0200

    Exercises from section 3.2.

--- Work/report.py
+++ Work/report.py
@@ -49,14 +49,19 @@ def make_report(portfolio, prices):
     return report
 
 
-#portfolio = read_portfolio('Data/portfolio.csv')
-portfolio = read_portfolio('Data/portfoliodate.csv')
-prices = read_prices('Data/prices.csv')
-report = make_report(portfolio, prices)
+def print_report(report):
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
+    print(('-' * 10 + ' ') * len(headers))
+    for name, shares, price, change in report:
+        print(f'{name:>10s} {shares:>10d} {"${:.2f}".format(price):>10s} {change:>10.2f}')
 
-# Print report
-headers = ('Name', 'Shares', 'Price', 'Change')
-print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
-print(('-' * 10 + ' ') * len(headers))
-for name, shares, price, change in report:
-    print(f'{name:>10s} {shares:>10d} {"${:.2f}".format(price):>10s} {change:>10.2f}')
+
+def portfolio_report(portfolio_filename, prices_filename):
+    portfolio = read_portfolio(portfolio_filename)
+    prices = read_prices(prices_filename)
+    report = make_report(portfolio, prices)
+    print_report(report)
+
+
+#portfolio_report('Data/portfolio.csv', 'Data/prices.csv')
